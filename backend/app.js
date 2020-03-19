36fea5c554f4ad4c63b9ea60a2f6d44a9ca979bb
captainstorm21 <tikana4@yahoo.com>
2020-03-18 21:54:22 -0700

    db connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require ('express');
-const app = express();
+// import mongoose
+const mongoose = require('mongoose');
 require ('dotenv').config()
 
+//app
+const app = express();
+//routes
 app.get('/', (req, res)=>{
     res.send ("hello from node")
 })
@@ -11,3 +15,19 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Our server is running on  ${port}`);
 })
+
+
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
